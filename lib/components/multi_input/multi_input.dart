1f0fda42fcd6280e9a06967343a46edadab34825
Patrick Minogue <patrick.minogue@gmail.com>
2019-04-03 16:18:09 +0200

    Added inputs and output to multi_input

--- lib/components/multi_input/multi_input.dart
+++ lib/components/multi_input/multi_input.dart
@@ -1,9 +1,26 @@
+import 'dart:async';
+
 import 'package:angular/angular.dart';
 
 @Component(
   selector: 'multi-input',
   templateUrl: 'multi_input.html',
 )
-class MultiInputComponent {
-  var name = 'Angular';
+class MultiInputComponent implements OnDestroy {
+  final StreamController<List<String>> _valueChangeController =
+      StreamController<List<String>>();
+
+  @Input()
+  String label;
+
+  @Input()
+  List<String> value = [];
+
+  @Output('valueChange')
+  Stream<List<String>> get valueChange => _valueChangeController.stream;
+
+  @override
+  void ngOnDestroy() {
+    _valueChangeController.close();
+  }
 }
