52a39d4913adf5868f837375981d7188c264f9c3
Patrick Minogue <patrick.minogue@gmail.com>
2019-04-04 14:52:43 +0200

    MultiInput now holds an actual textinput

--- lib/components/multi_input/multi_input_component.dart
+++ lib/components/multi_input/multi_input_component.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 
 import 'package:angular/angular.dart';
+import 'package:fo_components/components/fo_text_input/fo_text_input_component.dart';
 
 @Component(
-  selector: 'multi-input',
-  templateUrl: 'multi_input_component.html',
-)
+    selector: 'multi-input',
+    templateUrl: 'multi_input_component.html',
+    directives: [FoTextInputComponent])
 class MultiInputComponent implements OnDestroy {
   final StreamController<List<String>> _valueChangeController =
       StreamController<List<String>>();
