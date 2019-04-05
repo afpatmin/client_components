71f564bf863b3104506abcd6a28d88c9e9ffba3a
Patrick Minogue <patrick.minogue@gmail.com>
2019-04-05 12:25:51 +0200

    MultiInput now responds to enter press

--- lib/client_components.dart
+++ lib/client_components.dart
@@ -1,1 +1,6 @@
 library client_components;
+
+export 'components/app_layout/app_layout_component.dart';
+export 'components/gdpr_form/gdpr_form_component.dart';
+export 'components/login/login_component.dart';
+export 'components/multi_input/multi_input_component.dart';
--- lib/components/multi_input/multi_input_component.dart
+++ lib/components/multi_input/multi_input_component.dart
@@ -1,15 +1,49 @@
 import 'dart:async';
-
+import 'package:intl/intl.dart';
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:fo_components/components/fo_button/fo_button_component.dart';
 import 'package:fo_components/components/fo_text_input/fo_text_input_component.dart';
+import 'package:fo_components/pipes/capitalize_pipe.dart';
 
 @Component(
     selector: 'multi-input',
     templateUrl: 'multi_input_component.html',
-    directives: [FoTextInputComponent])
+    styleUrls: [
+      'multi_input_component.css'
+    ],
+    directives: [
+      formDirectives,
+      FoButtonComponent,
+      FoTextInputComponent,
+      NgFor
+    ],
+    pipes: [
+      CapitalizePipe
+    ])
 class MultiInputComponent implements OnDestroy {
   final StreamController<List<String>> _valueChangeController =
       StreamController<List<String>>();
+
+  final String msgAdd = Intl.message('add', name: 'add');
+
+  void onAdd() {
+    value ??= [];
+    value.add(model);
+    model = '';
+    _valueChangeController.add(value);
+  }
+
+  void onRemove(String v) {
+    value.remove(v);    
+    _valueChangeController.add(value);
+  }
+
+  void onChange(Object event) {
+    print(event);
+  }
+
+  String model;
 
   @Input()
   String label;
