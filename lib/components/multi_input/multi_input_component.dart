6782347abdea3df5cb380dcabf2182ba7d78c665
Patrick <patrick.minogue@gmail.com>
2021-04-27 21:53:14 +0000

    preventDefault

--- lib/components/multi_input/multi_input_component.dart
+++ lib/components/multi_input/multi_input_component.dart
@@ -59,8 +59,11 @@ class MultiInputComponent implements OnDestroy {
     }
   }
 
-  void onKeyDown(dom.Event event) {
-    event.stopPropagation();
+  void onKeyDown(dom.KeyEvent event) {
+    if (event.keyCode == dom.KeyCode.ENTER ||
+        event.keyCode == dom.KeyCode.MAC_ENTER) {
+      event.preventDefault();
+    }
   }
 
   void onRemove(String v) {
