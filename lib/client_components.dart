66e0c0c8a42d63136d72351fe76dc7c9db0a2456
Patrick Minogue <patrick.minogue@gmail.com>
2021-10-22 12:48:15 +0200

    removed AppLayoutComponent

--- lib/client_components.dart
+++ lib/client_components.dart
@@ -1,6 +1,5 @@
 library client_components;
 
-export 'components/app_layout/app_layout_component.dart';
 export 'components/gdpr_form/gdpr_form_component.dart';
 export 'components/login/login_component.dart';
 export 'components/multi_input/multi_input_component.dart';
